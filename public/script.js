0da0fc9f50ab13967fa403e356f3d7779b4c1d56
SHAURYAGUPTA01 <86559522+SHAURYAGUPTA01@users.noreply.github.com>
2024-06-26 19:41:51 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -107,24 +107,27 @@ $(function () {
         }
     })
 
-})
-
-$("#invite_button").click(function(){
-    let data = {
-        url : window.location.href,
-        to : to,
-    
-    }
-    $ajax({
-    url : "/send-name",
-    type : "post" ,
-    data : JSON.stringify(data),
-    datatype : 'json',
-    contentType : 'application/json',
-    success : function(result){
-        console.log(result.responseJSON)
-    }     
+    $("#invite_button").click(function () {
+        const to = prompt("Enter the email address")
+        let data = {
+            url: window.location.href,
+            to: to
+        }
+        $.ajax({
+            url: "/send-mail",
+            type: "post",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (result) {
+                alert("Invite sent!")
+            },
+            error: function (result) {
+                console.log(result.responseJSON)
+            }
+        })
     })
+
 })
 
 peer.on("open", (id) => {
